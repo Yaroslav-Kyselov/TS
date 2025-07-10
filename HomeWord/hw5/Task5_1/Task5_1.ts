02c41d6cf972394201bb08c4abefd5ee264ac099
Yaroslav-Kyselov <mmmyaroslav@gmail,com>
2025-07-10 20:18:08 +0300

    5 ts

---
+++ HomeWord/hw5/Task5_1/Task5_1.ts
@@ -0,0 +1,8 @@
+// створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
+
+function square51(aNum:number, bNum:number):number {
+    const result = aNum * bNum;
+    console.log(result);
+    return result;
+}
+console.log(square51(5,5))
